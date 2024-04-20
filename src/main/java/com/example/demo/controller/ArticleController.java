d3c428fa90871a859f81dd7241cc977b2bd645a5
jxehxn <wkdwogns2345@naver.com>
2024-04-20 16:30:38 +0900

    240420 16:30 commit

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -4,13 +4,20 @@ package com.example.demo.controller;
 import com.example.demo.dto.ArticleForm;
 import com.example.demo.entity.Article;
 import com.example.demo.repository.ArticleRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class ArticleController {
+    private static final Logger log = LoggerFactory.getLogger(ArticleController.class);
     @Autowired
     private ArticleRepository articleRepository;
 
@@ -32,6 +39,24 @@ public class ArticleController {
 
         return "";
     }
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id = "+id);
 
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article",articleEntity);
+
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model){
+        // 1. DB에서 모든 Article 데이터 가져오기
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+        // 2. 가져온 Article 묶음을 모델에 등록하기
+        model.addAttribute("articleList",articleEntityList);
+        // 3. 사용자에게 보여 줄 뷰 페이지 설정하기
+        return "articles/index";
+    }
 
 }
--- src/main/java/com/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -3,6 +3,9 @@ package com.example.demo.repository;
 import com.example.demo.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ArticleRepository extends CrudRepository<Article, Long> {
+import java.util.ArrayList;
 
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
